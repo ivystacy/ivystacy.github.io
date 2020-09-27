e0763d0f6701171112a576c2da092f175613b07f
ivystacy <41705343+ivystacy@users.noreply.github.com>
2020-09-26 18:24:12 -0700

    Add files via upload

--- aboutme.js
+++ aboutme.js
@@ -1,3 +1,26 @@
-function cutecat(){
-  document.getElementById('cutecat').style.display='block';
+function showProject() {
+  var x = document.getElementById("more");
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showProject2() {
+  var x = document.getElementById("more2");
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showProject3() {
+  var x = document.getElementById("more3");
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
